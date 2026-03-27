c99862df77b90ee60f5b2b6d4654c4ae44784c2d
Dominator007100 <domcadominator3@gmail.com>
2026-03-27 10:52:21 +0100

    Update db.ts

--- shared/db.ts
+++ shared/db.ts
@@ -5,7 +5,7 @@ import * as schema from "./schema"; // ← bez .ts
 const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing in .env");
+  throw new Error("DATABASE_URL is missing in environment variables");
 }
 
 const pool = new Pool({
